f4865099cfca86f45dc7f3ec9125ac54fe101a5f
root <root@aspire787.aspiresys.com>
2015-10-12 11:05:04 +0530

    Remove duplicated directory

--- oops_assignment/Config/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define( 'DB_HOST', '127.0.0.1');
-	define( 'DB_USER', 'root');
-	define( 'DB_PASSWORD', 'aspire@123');
-	define( 'DB_NAME', 'signup');
-	require_once "../Application/Views/Signup.php";
-?>
